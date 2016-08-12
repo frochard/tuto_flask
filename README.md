76b9a5262a2730f9c9d82d5395694ea37f730303
fred <fred@fred-portable.home>
2016-08-12 16:18:29 +0200

    add fix route bugs

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# tuto_flask
+# ocrci UI
 
--- app.py
+++ app.py
@@ -8,37 +8,29 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/running_wc/')
 def running_wc():
-    working_copies = ["wc1", "wc2"]
-    projects = ["Irma", "Axtdocprocess", "idocdb"]
-    return render_template('running_wc.html', title="OcrCI", working_copies=working_copies, projects=projects)
+    running_working_copies = ["wc1", "wc2"]
+    wc_projects = ["Irma", "Axtdocprocess", "idocdb"]
+    return render_template('running_wc.html', title="OcrCI", working_copies=running_working_copies, projects=wc_projects)
 
 
 @app.route('/working_copies/')
 def working_copies():
-    mail = "jean@bon.fr"
-    tel = "01 23 45 67 89"
-    return "Mail: {} --- Tel: {}".format(mail, tel)
+    return render_template('working_copies.html', title="OcrCI")
 
 
-@app.route('/aptly/', methods=['GET', 'POST'])
+@app.route('/aptly/')
 def aptly():
-    if request.method == 'POST':
-        return "Vous avez envoye : {msg}".format(msg=request.form['msg'])
-    return '<form action="" method="post"><input type="text" name="msg" /><input type="submit" value="Envoyer" /></form>'
+    return render_template('aptly.html', title="OcrCI")
 
 
 @app.route('/system/')
 def system():
-    mail = "jean@bon.fr"
-    tel = "01 23 45 67 89"
-    return "Mail: {} --- Tel: {}".format(mail, tel)
+    return render_template('system.html', title="OcrCI")
 
 
 @app.route('/debian_tools/')
 def debian_tools():
-    mail = "jean@bon.fr"
-    tel = "01 23 45 67 89"
-    return "Mail: {} --- Tel: {}".format(mail, tel)
+    return render_template('debian_tools.html', title="OcrCI")
 
 
 if __name__ == '__main__':
